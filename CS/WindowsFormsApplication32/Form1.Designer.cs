5a5b6b55b5b4a67e9eaead7823e0ed532e66844c
Bogdan Kharchenko <bogdan.kharchenko@devexpress.com>
2025-09-21 15:43:51 +0400

    Demo source code updated

--- CS/WindowsFormsApplication32/Form1.Designer.cs
+++ CS/WindowsFormsApplication32/Form1.Designer.cs
@@ -30,47 +30,43 @@ namespace WindowsFormsApplication32
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.components = new System.ComponentModel.Container();
-			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
-			this.imageCollection1 = new DevExpress.Utils.ImageCollection(this.components);
-			this.myTabbedMdiManager1 = new CustomTabbedMdiManager.MyTabbedMdiManager(this.components);
-			((System.ComponentModel.ISupportInitialize)(this.imageCollection1)).BeginInit();
-			((System.ComponentModel.ISupportInitialize)(this.myTabbedMdiManager1)).BeginInit();
-			this.SuspendLayout();
-			// 
-			// imageCollection1
-			// 
-			this.imageCollection1.ImageStream = ((DevExpress.Utils.ImageCollectionStreamer)(resources.GetObject("imageCollection1.ImageStream")));
-			this.imageCollection1.TransparentColor = System.Drawing.Color.Fuchsia;
-			this.imageCollection1.Images.SetKeyName(0, "cash.bmp");
-			this.imageCollection1.Images.SetKeyName(1, "clients.bmp");
-			// 
-			// myTabbedMdiManager1
-			// 
-			this.myTabbedMdiManager1.HeaderShowActiveFilesButton = true;
-			this.myTabbedMdiManager1.Images = this.imageCollection1;
-			this.myTabbedMdiManager1.MdiParent = this;
-			this.myTabbedMdiManager1.PageAdded += new DevExpress.XtraTabbedMdi.MdiTabPageEventHandler(this.myTabbedMdiManager1_PageAdded);
-			// 
-			// Form1
-			// 
-			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(501, 387);
-			this.IsMdiContainer = true;
-			this.Name = "Form1";
-			this.Text = "Form1";
-			this.Load += new System.EventHandler(this.Form1_Load);
-			((System.ComponentModel.ISupportInitialize)(this.imageCollection1)).EndInit();
-			((System.ComponentModel.ISupportInitialize)(this.myTabbedMdiManager1)).EndInit();
-			this.ResumeLayout(false);
+            this.components = new System.ComponentModel.Container();
+            this.svgImageCollection1 = new DevExpress.Utils.SvgImageCollection(this.components);
+            this.myTabbedMdiManager2 = new CustomTabbedMdiManager.MyTabbedMdiManager(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.svgImageCollection1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.myTabbedMdiManager2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // svgImageCollection1
+            // 
+            this.svgImageCollection1.Add("bo_employee", "image://svgimages/business objects/bo_employee.svg");
+            this.svgImageCollection1.Add("bo_department", "image://svgimages/business objects/bo_department.svg");
+            // 
+            // myTabbedMdiManager2
+            // 
+            this.myTabbedMdiManager2.Images = this.svgImageCollection1;
+            this.myTabbedMdiManager2.MdiParent = this;
+            this.myTabbedMdiManager2.PageAdded += new DevExpress.XtraTabbedMdi.MdiTabPageEventHandler(this.myTabbedMdiManager_PageAdded);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1002, 744);
+            this.IsMdiContainer = true;
+            this.Margin = new System.Windows.Forms.Padding(6);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.svgImageCollection1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.myTabbedMdiManager2)).EndInit();
+            this.ResumeLayout(false);
 
 		}
 
 		#endregion
-
-		private MyTabbedMdiManager myTabbedMdiManager1;
-		private DevExpress.Utils.ImageCollection imageCollection1;
-	}
+        private MyTabbedMdiManager myTabbedMdiManager2;
+        private DevExpress.Utils.SvgImageCollection svgImageCollection1;
+    }
 }
 
--- CS/WindowsFormsApplication32/Form1.cs
+++ CS/WindowsFormsApplication32/Form1.cs
@@ -10,7 +10,7 @@ namespace WindowsFormsApplication32
 		public Form1()
 		{
 			InitializeComponent();
-            this.myTabbedMdiManager1.HeaderShowActiveFilesButton = true;
+            this.myTabbedMdiManager2.HeaderShowActiveFilesButton = true;
 		}
 
 		private void Form1_Load(object sender, EventArgs e)
@@ -26,7 +26,7 @@ namespace WindowsFormsApplication32
 			xtraForm1.Show();
 		}
 
-		private void myTabbedMdiManager1_PageAdded(object sender, MdiTabPageEventArgs e)
+		private void myTabbedMdiManager_PageAdded(object sender, MdiTabPageEventArgs e)
 		{
 			XtraTabbedMdiManager tabbedMdi = (XtraTabbedMdiManager)sender;
 			e.Page.ImageIndex = tabbedMdi.Pages.Count - 1;
